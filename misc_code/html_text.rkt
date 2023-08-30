6f8dbd63b0f902e0d6c7e7c4b3ddede2e7dda3b4
Mark Watson <markw@markwatson.com>
2023-08-30 15:13:27 -0700

    -

--- misc_code/html_text.rkt
+++ misc_code/html_text.rkt
@@ -5,7 +5,7 @@
 (define (read-html-as-xexprs in)
   (caddr (xml->xexpr (element #f #f 'root '() (read-html-as-xml in)))))
 
-(define reddit (string->url "http://www.reddit.com/r/programming/search?q=racket&sort=relevance&restrict_sr=on&t=all"))
+(define reddit (string->url "http://markwatson.com"))
 
 (define xe (call/input-url reddit get-pure-port read-html-as-xexprs))
 
