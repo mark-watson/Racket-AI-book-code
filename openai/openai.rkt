e7ee88a18ee508e99f66136bec5964f0d0c03a35
Mark Watson <markw@markwatson.com>
2023-09-20 15:19:06 -0700

    -

--- openai/openai.rkt
+++ openai/openai.rkt
@@ -2,31 +2,13 @@
 
 (require net/http-easy)
 (require racket/set)
-
-(define (completion prompt)
-  (let* ((prompt-data (string-join (list "{\"prompt\": \"" prompt "\" ,\"max_tokens\": 120}")))
-         (temp0 (displayln prompt-data))
-         (auth (lambda (uri headers params)
-                 (values
-                  (hash-set* headers
-                             'authorization (string-join (list "Bearer " (getenv "OPENAI_API_KEY")))
-                             'content-type "application/json") params)))
-         (p
-          (post "https://api.openai.com/v1/engines/davinci/completions"
-                #:auth auth
-                #:data prompt-data))
-         (temp (println p))
-         (r (response-json p)))
-    (println r)
-    (hash-ref r 'choices)))
-
-(println (completion "Frank bought a new sports car. Frank drove"))
+(require pprint)
 
 (define (question prompt)
   (let* ((prompt-data (string-join (list "{\"messages\": [ {\"role\": \"user\", \"content\": \"Answer the question: "
                                          prompt
                                          "\"}], \"model\": \"gpt-3.5-turbo\"}")))
-         (temp0 (displayln prompt-data))
+         ;;(temp0 (displayln prompt-data))
          (auth (lambda (uri headers params)
                  (values
                   (hash-set* headers
@@ -36,10 +18,16 @@
           (post "https://api.openai.com/v1/chat/completions" ;; "https://api.openai.com/v1/engines/davinci/completions"
                 #:auth auth
                 #:data prompt-data))
-         (temp (println p))
+         ;;(temp (println p))
          (r (response-json p)))
-    (println r)
+    ;;(println r)
     (hash-ref (hash-ref (first (hash-ref r 'choices)) 'message) 'content)))
 
 (define answer2 (question "Mary is 30 and Harry is 25. Who is older?"))
 answer2
+
+
+(define (completion prompt)
+  (question (string-append "Continue writieng from the following text: " prompt)))
+
+(println (completion "Frank bought a new sports car. Frank drove"))
