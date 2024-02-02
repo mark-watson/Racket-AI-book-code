97e6c27c9387c3eced3d57b9a0daebc623d5751b
Mark Watson <markw@markwatson.com>
2024-02-02 11:27:55 -0700

    starting to add support for Travily search APIs

---
+++ search/tavily_search.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(require net/http-easy)
+(require racket/set)
+ (require racket/pretty)
+(require json)
+
+(provide tavily-search)
+
+(define (json-helper query)
+  (string-join
+   (list
+    "{\"api_key\": \"" (getenv "TAVILY_API_KEY")"\", "
+    "\"query\": \"" query "\", "
+    "\"max_results\": 5}")
+   ""))
+
+(define (filter-response a-hash-eq)
+  (list
+   (hash-ref a-hash-eq 'url)
+   (hash-ref a-hash-eq 'title)
+   (hash-ref a-hash-eq 'content)))
+
+(displayln (json-helper "1 + 2?"))
+                       
+
+(define (tavily-search query)
+  (let* ((prompt-data (json-helper query))
+         (p
+          (post
+           "https://api.tavily.com/search"
+           #:data prompt-data))
+         (r (response-json p)))
+    ;;(displayln prompt-data)
+    ;;(pretty-print r)
+    (map filter-response (hash-ref r  'results))))
+
+(pretty-print (tavily-search "Fun things to do in Sedona Arizona"))
